37734f6cda07498e1c2421efc769e9325ab60e7c
Dave <dave.dewire@gmail.com>
2016-11-20 23:47:20 +0000

    working

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 var express = require('express')
 var app = express()
+var fs = require('fs');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  displayForm(res);
+});
 
-app.listen(8080)
+//'myfile' is the name of the field in the form that has the file
+app.post('/', upload.single('myfile'), function (req, res, next) {
+  var r={"File Name": req.file.originalname,"File Size":req.file.size + " bytes"};
+  res.send(r);
+});
+
+app.listen(8080);
+
+function displayForm(res) {
+    fs.readFile('form.html', function (err, data) {
+        if (err) throw err;
+        else
+        {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+        }
+    });
+}
